6346c7418b8cd52c0e47c8b1d6c76b01c5fe6787
TheFrenchyDeveloper <kevin.massol@icloud.com>
2022-10-19 07:58:53 +0200

    Initial Commit

---
+++ ILoveSantorini/ILoveSantoriniApp.swift
@@ -0,0 +1,17 @@
+//
+//  ILoveSantoriniApp.swift
+//  ILoveSantorini
+//
+//  Created by KEVIN on 19/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct ILoveSantoriniApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
