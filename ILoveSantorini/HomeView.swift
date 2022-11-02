046301e394fa8473d812a28f096b88422f5c773e
TheFrenchyDeveloper <kevin.massol@icloud.com>
2022-11-02 16:26:35 +0100

    [FEAT] - Added header and assets

--- ILoveSantorini/HomeView.swift
+++ ILoveSantorini/HomeView.swift
@@ -8,14 +8,44 @@
 import SwiftUI
 
 struct HomeView: View {
+    
+    var color: Color {
+        let red: Double = 12 / 255
+        let green: Double = 95 / 255
+        let blue: Double = 175 / 255
+        return Color(red: red, green: green, blue: blue)
+    }
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            ZStack(alignment: .bottomTrailing) {
+                Image("bg")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(height: 225)
+                    .clipped()
+                HStack(alignment: .bottom) {
+                    Text("I 💙 Santorini")
+                        .font(.largeTitle)
+                        .fontWeight(.heavy)
+                        .foregroundColor(color)
+                    Spacer()
+                    Image("flag")
+                        .resizable()
+                        .frame(width: 75, height: 75)
+                }
+                .offset(y: 40)
+            }
+            Rectangle()
+                .frame(height: 40)
+                .foregroundColor(.clear)
+            Divider()
+                .frame(height: 2)
+                .overlay(color)
+                .padding(EdgeInsets.init(top: 0, leading: 15, bottom: 0, trailing: 15))
+            Spacer()
         }
-        .padding()
+        .edgesIgnoringSafeArea(.top)
     }
 }
 
