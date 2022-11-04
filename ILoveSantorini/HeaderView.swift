03936f7df8b91f9870b312d9b13fec68cd1690ea
TheFrenchyDeveloper <kevin.massol@icloud.com>
2022-11-04 11:18:51 +0100

    [FEAT] - Put the header in a separate file and change the color

---
+++ ILoveSantorini/HeaderView.swift
@@ -0,0 +1,47 @@
+//
+//  HeaderView.swift
+//  ILoveSantorini
+//
+//  Created by KEVIN on 04/11/2022.
+//
+
+import SwiftUI
+
+struct HeaderView: View {
+    var body: some View {
+        VStack {
+            ZStack(alignment: .bottomTrailing) {
+                Image("bg")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(height: 225)
+                    .clipped()
+                HStack(alignment: .bottom) {
+                    Text("I 💙 Santorini")
+                        .font(.largeTitle)
+                        .fontWeight(.heavy)
+                        .foregroundColor(Color("blue"))
+                    Spacer()
+                    Image("flag")
+                        .resizable()
+                        .frame(width: 75, height: 75)
+                }
+                .offset(y: 40)
+            }
+            Rectangle()
+                .frame(height: 40)
+                .foregroundColor(.clear)
+            Divider()
+                .frame(height: 2)
+                .overlay(Color("blue"))
+                .padding(EdgeInsets.init(top: 0, leading: 15, bottom: 0, trailing: 15))
+        }
+    }
+}
+
+struct HeaderView_Previews: PreviewProvider {
+    static var previews: some View {
+        HeaderView()
+            .previewLayout(.sizeThatFits)
+    }
+}
--- ILoveSantorini/HomeView.swift
+++ ILoveSantorini/HomeView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  HomeView.swift
 //  ILoveSantorini
 //
 //  Created by KEVIN on 19/10/2022.
@@ -9,41 +9,9 @@ import SwiftUI
 
 struct HomeView: View {
     
-    var color: Color {
-        let red: Double = 12 / 255
-        let green: Double = 95 / 255
-        let blue: Double = 175 / 255
-        return Color(red: red, green: green, blue: blue)
-    }
-    
     var body: some View {
         VStack {
-            ZStack(alignment: .bottomTrailing) {
-                Image("bg")
-                    .resizable()
-                    .aspectRatio(contentMode: .fill)
-                    .frame(height: 225)
-                    .clipped()
-                HStack(alignment: .bottom) {
-                    Text("I 💙 Santorini")
-                        .font(.largeTitle)
-                        .fontWeight(.heavy)
-                        .foregroundColor(color)
-                    Spacer()
-                    Image("flag")
-                        .resizable()
-                        .frame(width: 75, height: 75)
-                }
-                .offset(y: 40)
-            }
-            Rectangle()
-                .frame(height: 40)
-                .foregroundColor(.clear)
-            Divider()
-                .frame(height: 2)
-                .overlay(color)
-                .padding(EdgeInsets.init(top: 0, leading: 15, bottom: 0, trailing: 15))
-            Spacer()
+            HeaderView()
         }
         .edgesIgnoringSafeArea(.top)
     }
